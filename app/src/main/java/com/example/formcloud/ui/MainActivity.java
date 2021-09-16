6e16e020c0023b96a93d307603e14c1ca7dcd848
Leonardo Silva <81488877+leonardoprogrammer@users.noreply.github.com>
2021-09-16 15:47:14 -0300

    Reestruturação do projeto

--- app/src/main/java/com/example/formcloud/ui/MainActivity.java
+++ app/src/main/java/com/example/formcloud/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.formcloud;
+package com.example.formcloud.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.formcloud.R;
 
 public class MainActivity extends AppCompatActivity {
 
